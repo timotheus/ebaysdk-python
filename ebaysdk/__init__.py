eeeb00e2b562627730cb90a003a07e145ac5b4cf
Brian Gontowski <bgontowski@ebay.com>
2013-01-23 13:18:59 -0800

    Added parallel support through ebayparallel

--- ebaysdk/__init__.py
+++ ebaysdk/__init__.py
@@ -40,13 +40,14 @@ class ebaybase(object):
     >>> print dict2xml(d)
     <list>a</list><list>b</list><list>c</list>
     """
-    def __init__(self, debug=False, method='GET', proxy_host=None, timeout=20, proxy_port=80, **kwargs):
+    def __init__(self, debug=False, method='GET', proxy_host=None, timeout=20, proxy_port=80, parallel=None, **kwargs):
         self.verb       = None
         self.debug      = debug
         self.method     = method
         self.timeout    = timeout
         self.proxy_host = proxy_host
         self.proxy_port = proxy_port
+        self.parallel   = parallel
         self.spooled_calls = [];
         self._reset()
 
@@ -199,21 +200,22 @@ class ebaybase(object):
 
     def _execute_http_request(self):
         "performs the http post and returns the XML response body"
+
         response_data = ''
         try:
-            curl = pycurl.Curl()
+            self._curl = pycurl.Curl()
 
-            curl.setopt(pycurl.SSL_VERIFYPEER, 0)
-            curl.setopt(pycurl.SSL_VERIFYHOST, 0)
+            self._curl.setopt(pycurl.SSL_VERIFYPEER, 0)
+            self._curl.setopt(pycurl.SSL_VERIFYHOST, 0)
 
             if self.proxy_host:
-                curl.setopt(pycurl.PROXY, str('%s:%d' % (self.proxy_host, self.proxy_port)))
+                self._curl.setopt(pycurl.PROXY, str('%s:%d' % (self.proxy_host, self.proxy_port)))
             else:
-                curl.setopt(pycurl.PROXY, '')
+                self._curl.setopt(pycurl.PROXY, '')
 
             # construct headers
             request_headers = self._build_request_headers()
-            curl.setopt( pycurl.HTTPHEADER, [
+            self._curl.setopt( pycurl.HTTPHEADER, [
                 str( '%s: %s' % ( k, v ) ) for k, v in request_headers.items()
             ] )
 
@@ -228,41 +230,53 @@ class ebaybase(object):
 
             if self.method == 'POST':
                 request_xml = self._build_request_xml()
-                curl.setopt(pycurl.POST, True)
-                curl.setopt(pycurl.POSTFIELDS, str(request_xml))
+                self._curl.setopt(pycurl.POST, True)
+                self._curl.setopt(pycurl.POSTFIELDS, str(request_xml))
 
-            curl.setopt(pycurl.FOLLOWLOCATION, 1)
-            curl.setopt(pycurl.URL, str(request_url))
-            curl.setopt(pycurl.SSL_VERIFYPEER, 0)
+            self._curl.setopt(pycurl.FOLLOWLOCATION, 1)
+            self._curl.setopt(pycurl.URL, str(request_url))
+            self._curl.setopt(pycurl.SSL_VERIFYPEER, 0)
 
-            response_header = StringIO.StringIO()
-            response_body   = StringIO.StringIO()
+            self._response_header = StringIO.StringIO()
+            self._response_body   = StringIO.StringIO()
 
-            curl.setopt(pycurl.CONNECTTIMEOUT, self.timeout)
-            curl.setopt(pycurl.TIMEOUT, self.timeout)
+            self._curl.setopt(pycurl.CONNECTTIMEOUT, self.timeout)
+            self._curl.setopt(pycurl.TIMEOUT, self.timeout)
 
-            curl.setopt(pycurl.HEADERFUNCTION, response_header.write)
-            curl.setopt(pycurl.WRITEFUNCTION, response_body.write)
+            self._curl.setopt(pycurl.HEADERFUNCTION, self._response_header.write)
+            self._curl.setopt(pycurl.WRITEFUNCTION, self._response_body.write)
 
             if self.debug:
                 sys.stderr.write("CURL Request: %s\n" % request_url)
-                curl.setopt(pycurl.VERBOSE, 1)
-                curl.setopt(pycurl.DEBUGFUNCTION, self.debug_callback)
+                self._curl.setopt(pycurl.VERBOSE, 1)
+                self._curl.setopt(pycurl.DEBUGFUNCTION, self.debug_callback)
 
-            curl.perform()
-
-            response_code   = curl.getinfo(pycurl.HTTP_CODE)
-            response_status = response_header.getvalue().splitlines()[0]
-            response_reason = re.match( r'^HTTP.+? +\d+ +(.*) *$', response_status ).group(1)
-            response_data   = response_body.getvalue()
-
-            if response_code != 200:
-                self._response_error = "Error: %s" % response_reason
+            if self.parallel:
+                self.parallel._add_request(self)
+                return None
+            else:
+                self._curl.perform()
+                return self._process_http_request()
 
         except Exception, e:
             self._response_error = "Exception: %s" % e
+            raise Exception("%s" % e)
 
-        return response_data
+    def _process_http_request(self):
+        response_code   = self._curl.getinfo(pycurl.HTTP_CODE)
+        response_status = self._response_header.getvalue().splitlines()[0]
+        response_reason = re.match( r'^HTTP.+? +\d+ +(.*) *$', response_status ).group(1)
+        response_data   = self._response_body.getvalue()
+
+        self._response_header = None
+        self._response_body   = None
+        self._curl.close()
+
+        if response_code != 200:
+            self._response_error = "Error: %s" % response_reason
+            raise Exception('%s' % response_reason)
+        else:
+            return response_data
 
     # Child classes should override this method based on what the errors in the
     # XML response body look like. They can choose to look at the 'ack',
@@ -433,12 +447,12 @@ class html(ebaybase):
         "performs the http post and returns the XML response body"
 
         try:
-            curl = pycurl.Curl()
+            self._curl = pycurl.Curl()
 
             if self.proxy_host:
-                curl.setopt(pycurl.PROXY, str('%s:%d' % (self.proxy_host, self.proxy_port)))
+                self._curl.setopt(pycurl.PROXY, str('%s:%d' % (self.proxy_host, self.proxy_port)))
             else:
-                curl.setopt(pycurl.PROXY, '')
+                self._curl.setopt(pycurl.PROXY, '')
 
             request_url = self.url
             if self.call_data and self.method == 'GET':
@@ -446,39 +460,33 @@ class html(ebaybase):
 
             if self.method == 'POST':
                 request_xml = self._build_request_xml()
-                curl.setopt(pycurl.POST, True)
-                curl.setopt(pycurl.POSTFIELDS, str(request_xml))
+                self._curl.setopt(pycurl.POST, True)
+                self._curl.setopt(pycurl.POSTFIELDS, str(request_xml))
 
-            curl.setopt(pycurl.FOLLOWLOCATION, 1)
-            curl.setopt(pycurl.URL, str(request_url))
-            curl.setopt(pycurl.SSL_VERIFYPEER, 0)
+            self._curl.setopt(pycurl.FOLLOWLOCATION, 1)
+            self._curl.setopt(pycurl.URL, str(request_url))
+            self._curl.setopt(pycurl.SSL_VERIFYPEER, 0)
 
-            response_header = StringIO.StringIO()
-            response_body   = StringIO.StringIO()
+            self._response_header = StringIO.StringIO()
+            self._response_body   = StringIO.StringIO()
 
-            curl.setopt(pycurl.CONNECTTIMEOUT, self.timeout)
-            curl.setopt(pycurl.TIMEOUT, self.timeout)
+            self._curl.setopt(pycurl.CONNECTTIMEOUT, self.timeout)
+            self._curl.setopt(pycurl.TIMEOUT, self.timeout)
 
-            curl.setopt(pycurl.HEADERFUNCTION, response_header.write)
-            curl.setopt(pycurl.WRITEFUNCTION, response_body.write)
+            self._curl.setopt(pycurl.HEADERFUNCTION, self._response_header.write)
+            self._curl.setopt(pycurl.WRITEFUNCTION, self._response_body.write)
 
             if self.debug:
                 sys.stderr.write("CURL Request: %s\n" % request_url)
-                curl.setopt(pycurl.VERBOSE, 1)
-                curl.setopt(pycurl.DEBUGFUNCTION, self.debug_callback)
+                self._curl.setopt(pycurl.VERBOSE, 1)
+                self._curl.setopt(pycurl.DEBUGFUNCTION, self.debug_callback)
 
-            curl.perform()
-
-            response_code   = curl.getinfo(pycurl.HTTP_CODE)
-            response_status = response_header.getvalue().splitlines()[0]
-            response_reason = re.match( r'^HTTP.+? +\d+ +(.*) *$', response_status ).group(1)
-            response_data   = response_body.getvalue()
-
-            if response_code != 200:
-                self._response_error = "Error: %s" % response_reason
-                raise Exception('%s' % response_reason)
+            if self.parallel:
+                self.parallel._add_request(self)
+                return None
             else:
-                return response_data
+                self._curl.perform()
+                return self._process_http_request()
 
         except Exception, e:
             self._response_error = "Exception: %s" % e
@@ -776,3 +784,51 @@ class SOAService( ebaybase ):
         else:
             soap = xml
         return soap
+
+class ebayparallel(object):
+    _requests = None
+
+    def __init__(self):
+        self._requests = []
+        self._errors = []
+
+    def _add_request(self, request):
+        self._requests.append(request)
+
+    def wait(self, timeout=20):
+        "wait for all of the api requests to complete"
+
+        self._errors = []
+        try:
+            if timeout > 0:
+                multi = pycurl.CurlMulti()
+                for request in self._requests:
+                    multi.add_handle(request._curl)
+                while True:
+                    while True:
+                        ret, num = multi.perform()
+                        if ret != pycurl.E_CALL_MULTI_PERFORM:
+                            break
+                    if num == 0:
+                        break
+                    if multi.select(timeout) < 0:
+                        raise pycurl.error(pycurl.E_OPERATION_TIMEOUTED)
+                for request in self._requests:
+                    multi.remove_handle(request._curl)
+                    request._response_content = request._process_http_request()
+                    if request._response_error:
+                        self._errors.append(request._response_error)
+                    self._errors.extend(request._get_resp_body_errors())
+                multi.close()
+            self._requests = []
+        except Exception, e:
+            self._errors.append("Exception: %s" % e)
+            raise Exception("%s" % e)
+
+    def error(self):
+        "builds and returns the api error message"
+
+        if len(self._errors) > 0:
+            return "parallel error:\n%s\n" % ("\n".join(self._errors))
+
+        return ""
