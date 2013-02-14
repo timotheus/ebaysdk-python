08a07eacb7b6e5c224f2e9cf249657b1bcfc5d3d
Tim Keefer <tkeefer@gmail.com>
2013-02-14 13:37:30 -0800

    add sample with xml attribute

--- samples/finding.py
+++ samples/finding.py
@@ -13,7 +13,7 @@ def init_options():
 	parser.add_option("-d", "--debug",
 	    			  action="store_true", dest="debug", default=False,
 					  help="Enabled debugging [default: %default]")
-	parser.add_option("-y", "--yaml",#
+	parser.add_option("-y", "--yaml",
 	                  dest="yaml", default='ebay.yaml',
 		              help="Specifies the name of the YAML defaults file. [default: %default]")
 	parser.add_option("-a", "--appid",
@@ -27,7 +27,21 @@ def run(opts):
 	api = finding(debug=opts.debug, appid=opts.appid, config_file=opts.yaml) 
 	api.execute('findItemsAdvanced', {'keywords': 'python'})
 
-	print "Finding samples for SDK version %s" % ebaysdk.get_version()
+	if api.error():
+		raise Exception(api.error())
+
+	if api.response_content():
+		print "Call Success: %s in length" % len(api.response_content())
+
+	print "Response code: %s" % api.response_code()
+	print "Response DOM: %s" % api.response_dom()
+
+	dictstr = "%s" % api.response_dict()
+	print "Response dictionary: %s..." % dictstr[:50]
+
+def run2(opts):
+	api = finding(debug=opts.debug, appid=opts.appid, config_file=opts.yaml) 
+	api.execute('findItemsByProduct', '<productId type="ReferenceID">53039031</productId>')
 
 	if api.error():
 		raise Exception(api.error())
@@ -42,6 +56,8 @@ def run(opts):
 	print "Response dictionary: %s..." % dictstr[:50]
 
 if __name__ == "__main__":
-    (opts, args) = init_options()
-    run(opts)
+	print "Finding samples for SDK version %s" % ebaysdk.get_version()
+	(opts, args) = init_options()
+	run(opts)
+	run2(opts)
 
