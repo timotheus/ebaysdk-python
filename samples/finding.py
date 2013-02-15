00ac32f51ad551819e2f5f0c9869c526ac56bba1
Tim Keefer <tkeefer@gmail.com>
2013-02-14 16:19:45 -0800

    editor litters some tabs

--- samples/finding.py
+++ samples/finding.py
@@ -7,57 +7,57 @@ import ebaysdk
 from ebaysdk import finding
 
 def init_options():
-	usage = "usage: %prog [options]"
-	parser = OptionParser(usage=usage)
+    usage = "usage: %prog [options]"
+    parser = OptionParser(usage=usage)
 
-	parser.add_option("-d", "--debug",
-	    			  action="store_true", dest="debug", default=False,
-					  help="Enabled debugging [default: %default]")
-	parser.add_option("-y", "--yaml",
-	                  dest="yaml", default='ebay.yaml',
-		              help="Specifies the name of the YAML defaults file. [default: %default]")
-	parser.add_option("-a", "--appid",
-		              dest="appid", default=None,
-		              help="Specifies the eBay application id to use.")
+    parser.add_option("-d", "--debug",
+                      action="store_true", dest="debug", default=False,
+                      help="Enabled debugging [default: %default]")
+    parser.add_option("-y", "--yaml",
+                      dest="yaml", default='ebay.yaml',
+                      help="Specifies the name of the YAML defaults file. [default: %default]")
+    parser.add_option("-a", "--appid",
+                      dest="appid", default=None,
+                      help="Specifies the eBay application id to use.")
 
-	(opts, args) = parser.parse_args()
-	return opts, args
+    (opts, args) = parser.parse_args()
+    return opts, args
 
 def run(opts):
-	api = finding(debug=opts.debug, appid=opts.appid, config_file=opts.yaml) 
-	api.execute('findItemsAdvanced', {'keywords': 'python'})
+    api = finding(debug=opts.debug, appid=opts.appid, config_file=opts.yaml) 
+    api.execute('findItemsAdvanced', {'keywords': 'python'})
 
-	if api.error():
-		raise Exception(api.error())
+    if api.error():
+        raise Exception(api.error())
 
-	if api.response_content():
-		print "Call Success: %s in length" % len(api.response_content())
+    if api.response_content():
+        print "Call Success: %s in length" % len(api.response_content())
 
-	print "Response code: %s" % api.response_code()
-	print "Response DOM: %s" % api.response_dom()
+    print "Response code: %s" % api.response_code()
+    print "Response DOM: %s" % api.response_dom()
 
-	dictstr = "%s" % api.response_dict()
-	print "Response dictionary: %s..." % dictstr[:50]
+    dictstr = "%s" % api.response_dict()
+    print "Response dictionary: %s..." % dictstr[:50]
 
 def run2(opts):
-	api = finding(debug=opts.debug, appid=opts.appid, config_file=opts.yaml) 
-	api.execute('findItemsByProduct', '<productId type="ReferenceID">53039031</productId>')
+    api = finding(debug=opts.debug, appid=opts.appid, config_file=opts.yaml) 
+    api.execute('findItemsByProduct', '<productId type="ReferenceID">53039031</productId>')
 
-	if api.error():
-		raise Exception(api.error())
+    if api.error():
+        raise Exception(api.error())
 
-	if api.response_content():
-		print "Call Success: %s in length" % len(api.response_content())
+    if api.response_content():
+        print "Call Success: %s in length" % len(api.response_content())
 
-	print "Response code: %s" % api.response_code()
-	print "Response DOM: %s" % api.response_dom()
+    print "Response code: %s" % api.response_code()
+    print "Response DOM: %s" % api.response_dom()
 
-	dictstr = "%s" % api.response_dict()
-	print "Response dictionary: %s..." % dictstr[:50]
+    dictstr = "%s" % api.response_dict()
+    print "Response dictionary: %s..." % dictstr[:50]
 
 if __name__ == "__main__":
-	print "Finding samples for SDK version %s" % ebaysdk.get_version()
-	(opts, args) = init_options()
-	run(opts)
-	run2(opts)
+    print "Finding samples for SDK version %s" % ebaysdk.get_version()
+    (opts, args) = init_options()
+    run(opts)
+    run2(opts)
 
--- tests/__init__.py
+++ tests/__init__.py
@@ -4,10 +4,10 @@ import ebaysdk
 import os
 
 def getTestSuite():
-	suite = unittest.TestSuite()
+    suite = unittest.TestSuite()
 
-	suite.addTest(doctest.DocTestSuite(ebaysdk))
-	return suite
+    suite.addTest(doctest.DocTestSuite(ebaysdk))
+    return suite
 
 runner = unittest.TextTestRunner()
 runner.run(getTestSuite())
