d9411f206ce2e57e99cca2f46bf779956b69669f
Andy Raines <andrew.raines@gmail.com>
2014-02-19 22:27:20 +0000

    Bug fix to shopping api header so that siteid can be set

--- ebaysdk/shopping/__init__.py
+++ ebaysdk/shopping/__init__.py
@@ -89,7 +89,7 @@ class Connection(BaseConnection):
         headers = {
             "X-EBAY-API-VERSION": self.config.get('version', ''),
             "X-EBAY-API-APP-ID":  self.config.get('appid', ''),
-            "X-EBAY-API-SITEID":  self.config.get('siteid', ''),
+            "X-EBAY-API-SITE-ID":  self.config.get('siteid', ''),
             "X-EBAY-API-CALL-NAME": verb,
             "X-EBAY-API-REQUEST-ENCODING": "XML",
             "Content-Type": "text/xml"
