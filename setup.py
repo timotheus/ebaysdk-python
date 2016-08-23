df553c0ea3e42aa5fbe190ca2e46d195bec4e828
Tim Keefer <tkeefer@gmail.com>
2016-08-23 11:29:01 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -47,5 +47,11 @@ setup(
     classifiers=[
         'Topic :: Internet :: WWW/HTTP',
         'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
     ]
 )
