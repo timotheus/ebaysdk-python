f6a32b814fdb5c043cab243175aaeeb108063fcb
Tim Keefer <tkeefer@gmail.com>
2016-08-24 10:45:16 -0700

    update xmlns for policy api

--- ebaysdk/policies/__init__.py
+++ ebaysdk/policies/__init__.py
@@ -126,7 +126,7 @@ class Connection(BaseConnection):
 
     def build_request_data(self, verb, data, verb_attrs):
         xml = "<?xml version='1.0' encoding='utf-8'?>"
-        xml += "<{verb}Request xmlns=\"http://www.ebay.com/marketplace/selling\">".format(verb=verb)
+        xml += "<{verb}Request xmlns=\"http://www.ebay.com/marketplace/selling/v1/services\">".format(verb=verb)
         xml += dict2xml(data, self.escape_xml)
         xml += "</{verb}Request>".format(verb=verb)
 
